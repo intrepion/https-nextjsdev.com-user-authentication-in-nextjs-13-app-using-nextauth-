c93c84490807e6908728fb63c41827785e81b0d3
Oliver Forral <intrepion@gmail.com>
2024-04-15 15:10:30 -0600

    Creating Components for Our App

--- nextauth-project/src/app/layout.tsx
+++ nextauth-project/src/app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NextAuth Tutorial- By NextjsDev",
+  title: "NextAuth Tutorial",
   description: "User Authentication in Next.js 13 App using NextAuth",
 };
 
@@ -16,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex justify-center items-start p-6 min-h-screen">
-          {children}
-        </main>
+        <AuthProvider>
+          <Navbar />
+          <main className="flex justify-center items-start p-6 h-[calc(100vh_-_80px)] bg-gray-900">
+            {children}
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
--- nextauth-project/src/app/page.tsx
+++ nextauth-project/src/app/page.tsx
@@ -1,14 +1,19 @@
-import { options } from "./api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
+import { options } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import UserCard from "./components/UserCard";
 
 export default async function Home() {
   const session = await getServerSession(options);
 
   return (
     <>
-      <h1 className="text-5xl text-red-600 font-semibold">
-        You are Not Authenticated !!
-      </h1>
+      {session ? (
+        <UserCard user={session?.user} pagetype={"Home"} />
+      ) : (
+        <h1 className="text-5xl text-red-600 font-semibold">
+          You are Not Authenticated !!
+        </h1>
+      )}
     </>
   );
 }
