78b3da82c956be961558e4cb95f332b23cb5cbed
Oliver Forral <intrepion@gmail.com>
2024-04-15 15:05:28 -0600

    Setting Up Authentication with NextAuth.js

--- nextauth-project/src/app/page.tsx
+++ nextauth-project/src/app/page.tsx
@@ -1,3 +1,6 @@
+import { options } from "./api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+
 export default async function Home() {
   const session = await getServerSession(options);
 
